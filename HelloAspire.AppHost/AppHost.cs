abac3f14f5d20aca4b030f50ea6c362cb5693f73
vickevire909 <vwesstrom@gmail.com>
2026-09-05 10:35:17 +0000

    chore: delete template web frontend

--- HelloAspire.AppHost/AppHost.cs
+++ HelloAspire.AppHost/AppHost.cs
@@ -1,18 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var cache = builder.AddRedis("cache");
-
-var apiService = builder
+var _ = builder
     .AddProject<Projects.HelloAspire_ApiService>("apiservice")
     .WithHttpHealthCheck("/health");
-
-builder
-    .AddProject<Projects.HelloAspire_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithHttpHealthCheck("/health")
-    .WithReference(cache)
-    .WaitFor(cache)
-    .WithReference(apiService)
-    .WaitFor(apiService);
 
 builder.Build().Run();
